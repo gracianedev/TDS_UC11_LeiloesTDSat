a7ec36211a55eace56d503536741cc0e580f3015
Graciane da Silva <graciane.dev@gmail.com>
2024-12-21 11:24:24 -0300

    Definida a tela principal da aplicação (tela inicial: cadastroVIEW)

---
+++ LeiloesTDSat/src/TelaPrincipal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author GFS_Mac
+ */
+public class TelaPrincipal {
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+    cadastroVIEW telaPrincipal = new cadastroVIEW ();
+    telaPrincipal.setVisible(true);
+            }
+            });
+}
+}
